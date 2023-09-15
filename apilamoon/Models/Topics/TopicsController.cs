18c5767ca79d0a0f48fab4ca55946c75c0679fae
Felipe Moran <felipe.r.moran@gmail.com>
2023-09-15 14:04:23 -0300

    Topics

---
+++ apilamoon/Models/Topics/TopicsController.cs
@@ -0,0 +1,90 @@
+using MainProfiles.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace MainProfiles.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TopicController : ControllerBase
+{
+    IMongoCollection<Topic> _topic;
+    public TopicController(MeuMongo meuMongo)
+    {
+        _topic = meuMongo.DB.GetCollection<Topic>("topics");
+    }
+
+    //Faz a consulta de todos os Usuarios cadastrados
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var topics = _topic.AsQueryable().ToList();
+        return Ok(topics);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Topic topic)
+    {
+        _topic.InsertOne(topic);
+        return Ok();
+    }
+
+    [HttpGet("referencecourse/{referencecourse}")]
+    //Faz a consulta de apenas 1 usuário, com o cpf
+    public ActionResult<Topic> GetByReferenceCourse(string referencecourse)
+    {
+        var topic = _topic.Find(Topic => Topic.ReferenceCourse == referencecourse).FirstOrDefault();
+        if (topic == null)
+        {
+            return NotFound();
+        }
+        return Ok(topic);
+    }
+
+    [HttpGet("title/{title}")]
+    //Faz a consulta de apenas 1 usuário, com o cpf
+    public ActionResult<Topic> GetByTitle(string title)
+    {
+        var topic = _topic.Find(Topic => Topic.Title == title).FirstOrDefault();
+        if (topic == null)
+        {
+            return NotFound();
+        }
+        return Ok(topic);
+    }
+
+    [HttpPut("{title}")]
+    public IActionResult PutByTitle(string title, Topic updatedTopic)
+    {
+        var filter = Builders<Topic>.Filter.Eq(u => u.Title, title);
+        var update = Builders<Topic>.Update
+            .Set(u => u.Title, updatedTopic.Title)
+            .Set(u => u.Body, updatedTopic.Body)
+            .Set(u => u.Attachments, updatedTopic.Attachments)
+            .Set(u => u.User_Comments, updatedTopic.User_Comments);
+        var updateResult = _topic.UpdateOne(filter, update);
+
+        if (updateResult.ModifiedCount == 0)
+        {
+            return NotFound();
+        }
+
+        return Ok();
+    }
+
+    // HTTP DELETE por CPF
+    [HttpDelete("{title}")]
+    public IActionResult DeleteByTitle(string title)
+    {
+        var filter = Builders<Topic>.Filter.Eq(u => u.Title, title);
+        var deleteResult = _topic.DeleteOne(filter);
+
+        if (deleteResult.DeletedCount == 0)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+}
