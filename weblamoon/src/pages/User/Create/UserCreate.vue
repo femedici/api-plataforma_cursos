8b387009510c396bedf32aa716883292cff87d8e
Mattok <mattokplays@gmail.com>
2023-09-19 01:26:03 -0300

    CSSs sendo ajustados

--- weblamoon/src/pages/User/Create/UserCreate.vue
+++ weblamoon/src/pages/User/Create/UserCreate.vue
@@ -1,20 +1,21 @@
 <template>
-    <div>
+    <div class="user-form">
         <form @submit="submitForm">
-            <label for="nome">Cpf:</label>
-            <input type="text" id="cpf" v-model="formData.cpf" required>
+            <label for="nome" labels>Cpf</label>
+            <input type="text" id="search-bars" v-model="formData.cpf" required>
 
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" v-model="formData.name" required>
+            <label for="nome" labels><br>Nome</label>
+            <input type="text" id="search-bars" v-model="formData.name" required>
 
-            <label for="email">Email:</label>
-            <input type="email" id="email" v-model="formData.email" required>
+            <label for="email" labels><br>Email</label>
+            <input type="email" id="search-bars" v-model="formData.email" required>
 
-            <label for="password">Senha:</label>
-            <input type="text" id="password" v-model="formData.password" required>
-
-            <button type="submit">Enviar</button>
-
+            <label for="password" labels><br>Senha</label>
+            <input type="text" id="search-bars" v-model="formData.password" required>
+            <br>
+            <div class="centralize">
+                <button type="submit" id="centralize">Enviar</button>
+            </div>
             <div v-if="error === false">
                 <p>Sucesso ao cadastrar usuário</p>
             </div>
