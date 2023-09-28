345960679f17b7464b53c86dbccc6e98d7c3442a
Felipe Medici <ic1dem.contato@gmail.com>
2023-09-27 23:46:47 -0300

    login

--- weblamoon/src/pages/LoginPage.vue
+++ weblamoon/src/pages/LoginPage.vue
@@ -25,14 +25,6 @@
                     density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline"
                     variant="outlined" @click:append-inner="visible = !visible"></v-text-field>
 
-                <v-card class="mb-12" color="surface-variant" variant="tonal">
-                    <v-card-text class="text-medium-emphasis text-caption">
-                        Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three
-                        hours. If you must login now, you can also click "Forgot login password?" below to reset the login
-                        password.
-                    </v-card-text>
-                </v-card>
-
                 <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
                     Log In
                 </v-btn>
