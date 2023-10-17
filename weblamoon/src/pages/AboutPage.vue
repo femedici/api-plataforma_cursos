c41d15c60ad6e8841120bba280bc8694b918e534
mattokgit44 <mattokplays@gmail.com>
2023-10-17 13:41:18 -0300

    Add pop-up change pic & reformulated about-us page

--- weblamoon/src/pages/AboutPage.vue
+++ weblamoon/src/pages/AboutPage.vue
@@ -1,54 +1,130 @@
 <template>
     <div class="about-us">
+        <v-parallax
+        class="object-cover"
+        src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+        >
+            <div class="header">
+                <h2><strong>Sobre nós</strong></h2>
+                <v-img class="logoimg" src="@/assets/logoName.png"></v-img>
+            </div>        
+</v-parallax>
       <div class="about-content">
         <div class="about-section" v-for="(topic, key, index) in topics" :key="key">
           <div class="about-card">
-            <h1>{{ topic.title }}</h1>
-            <v-divider></v-divider>
-            <p>{{ topic.text }}</p>
-        </div>
-        <v-divider class="divider" v-if="index < Object.keys(topics).length - 1"></v-divider>
+            <div class="about-card-content" :class="{'reverse': index % 2 !== 0}">
+              <div class="text-container">
+                <h1>{{ topic.title }}</h1>
+                <v-divider></v-divider>
+                <p>{{ topic.text }}</p>
+              </div>
+              <img :src="topic.image" alt="Imagem para {{ topic.title }}" class="about-image">
+            </div>
+            <v-divider class="divider" v-if="index < Object.keys(topics).length - 1"></v-divider>
+          </div>
         </div>
       </div>
+      <div class="github-users">
+        <h2>Equipe</h2>
+      <div class="github-user" v-for="(user, userIndex) in Users" :key="userIndex">
+        <img :src="user.image">
+        <div class="user-details">
+            <a :href="`https://github.com/${user.githubUsername}`" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/></a>
+          <p>{{ user.name }}</p>
+        </div>
+      </div>
+    </div>
     </div>
   </template>
   
   <script>
+  
   export default {
+    
     data() {
       return {
         topics: {
-          "sobre-nos": {
-            title: "Sobre Nós",
-            text: "Bem-vindo à LaMoon, a plataforma líder em educação online! Nossa missão é tornar a aprendizagem acessível, conveniente e eficaz para todos, em qualquer lugar do mundo."
-          },
-          "nossa-equipe": {
-            title: "Nossa Equipe",
-            text: "Nossa equipe é composta por educadores, desenvolvedores, designers e entusiastas da educação. Estamos dedicados a criar a melhor experiência de aprendizagem online para nossos alunos. Se você tiver alguma dúvida ou precisar de suporte, nossa equipe de suporte está sempre pronta para ajudar."
+          "sobre-lamoon": {
+            title: "Sobre a LaMoon",
+            text: "Nossa plataforma vai permitir que você compre cursos sobre os tópicos que mais lhe interessam. Mas a melhor parte é que, se você é um especialista ou tem conhecimento valioso a compartilhar, você pode criar seu próprio curso e vendê-lo. É uma oportunidade única para todos, sejam alunos ávidos ou futuros instrutores. Mas claro, todos os cursos são verificados antes de aprovados, além dos usuarios avaliarem a qualidade dos mesmos. Estamos desenvolvendo essa plataforma como parte de nosso projeto para a matéria de Projeto Integrador da UTFPR-CM no segundo semestre de 2023.",
+            image: "https://images.unsplash.com/photo-1546410531-bb4caa6b424d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80"
           },
           "missao": {
             title: "Missão",
-            text: "Nossa missão é capacitar indivíduos através da educação, tornando o conhecimento e as habilidades acessíveis a todos. Acreditamos que a educação é a chave para desbloquear oportunidades ilimitadas e estamos comprometidos em tornar a aprendizagem online eficaz e envolvente."
+            text: "Nossa missão é capacitar indivíduos através da educação, tornando o conhecimento e as habilidades acessíveis a todos. Acreditamos que a educação é a chave para desbloquear oportunidades ilimitadas e estamos comprometidos em tornar a aprendizagem online eficaz e envolvente.",
+            image: "https://media.istockphoto.com/id/1418476503/pt/foto/businessman-analyzing-companys-financial-balance-sheet-working-with-digital-augmented-reality.jpg?s=612x612&w=0&k=20&c=XzJYWhzF6b37CxAwUQdDMduRxdnxhVaiId1W8Qmk8TE="
           },
-          "visao": {
-            title: "Visão",
-            text: "Nossa visão é ser a principal plataforma global de educação online, oferecendo uma ampla gama de cursos de alta qualidade e recursos educacionais para alunos em todo o mundo. Estamos constantemente inovando e expandindo nossa oferta de cursos para atender às necessidades em evolução de nossos alunos."
-          }
-        }
-      };
-    }
-  };
-  </script>
+          "nossa-equipe": {
+            title: "Linguagens e Tecnologias",
+            text: "Para o desenvolvimento da plataforma foram utilizados, para o Front-End, as linguagens Vue, HTML5 e CSS3, enquanto para o Back-End, as linguagens C#, dotNet e MongoDB.",
+            image: "https://images.unsplash.com/photo-1593062037896-764e9f52029e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          },
+        },
+        Users: [
+        {
+          name: "Felipe Augusto Medici",
+          image: "https://avatars.githubusercontent.com/u/111476568?v=4",
+          githubUsername: "femedici",
+        },
+        {
+          name: "Felipe Roskosz Moran",
+          image: "",
+          githubUsername: "feliperm17",
+        },
+        {
+          name: "Felipe Martins Sanches",
+          image: "https://avatars.githubusercontent.com/u/79864909?s=400&u=0bd727ea4080ae444b74f08fac8cad2d17912821&v=4",
+          githubUsername: "mattokgit44",
+        },
+        {
+          name: "Caio Ducati Castilho",
+          image: "URL_DA_IMAGEM_DO_GITHUB_USER_4",
+          githubUsername: "Cast43",
+        },
+      ],
+    };
+  },
+};
+</script>
   
   <style scoped>
-  .about-us {
+  .logoimg{
+    max-height:20%;
+    max-width:20%;
+    margin-left:41%;
+    margin-top:-20  px;
+}
+  h2{
+    font-size:xx-large;
+  }
+  .header{
+    text-align: center;
+    margin-top: 175px;
+  }
+.object-cover {
+    max-height: 400px;
+}
+
+.about-us {
     text-align: center;
   }
   
   .about-card {
     background-color: #0a0736;
-    border-radius: 8px;
+    /*border-radius: 8px;*/
     padding: 20px;
+  }
+  
+  .about-card-content {
+    display: flex;
+    align-items: center;
+    text-align: left;
+  }
+  
+  .about-image {
+    max-width: 500px;
+    border-radius: 8px;
+    margin-left: 20px;
   }
   
   .divider {
@@ -56,10 +132,56 @@
     background-color: #ccc;
     margin: 10px 0;
   }
+  
   h1 {
-    font-size:x-large;
+    font-size: x-large;
   }
-  p{
+  
+  .text-container {
+    flex: 1;
+  }
+  
+  p {
     margin-top: 30px;
   }
-  </style>
+  
+  .reverse {
+    flex-direction: row-reverse;
+    text-align: right;
+  }
+  
+  .reverse .about-image {
+    margin-left: 20px;
+    margin-right: 0;
+  }
+  .diminuir{
+    max-width: 3%;
+    max-height: 3%;
+  }
+  .github-users {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+}
+
+.github-user {
+  text-align: center;
+  margin-bottom: 30px;
+  margin-top: 20px;
+}
+
+.github-user img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+}
+
+.user-details img {
+  width: 24px;
+  height: 24px; 
+}
+
+.user-details p {
+  margin-top: 10px;
+}
+</style>
--- weblamoon/src/pages/User/UserView.vue
+++ weblamoon/src/pages/User/UserView.vue
@@ -2,7 +2,10 @@
     <div class="user-profile">
       <div class="user-details">
         <h1>Dados do Usuário</h1>
-        <img class="profile-image" src="https://static.vecteezy.com/system/resources/previews/020/911/739/non_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png" alt="Foto de Perfil">
+        <div class="profile-container">
+            <img class="profile-image" :src="currentProfileImageUrl" alt="Foto de Perfil">
+          <span class="edit-profile-icon" @click="openEditProfilePopup">✎</span>
+        </div>
         <p class="text-left"><strong>Nome do usuário:</strong> {{ getUserName }}</p>
         <p class="text-left"><strong>Email do usuário:</strong> {{ getUserEmail }}</p>
         <p class="text-left"><strong>Data de criação do usuário:</strong> {{ getUserDate }}</p>
@@ -12,94 +15,114 @@
         <router-link :to="'/alter-user/' + user.cpf" class="button">Alterar Usuário</router-link>
         <button type="button" @click="confirmDeleteUser" class="button delete-button">Deletar Usuário</button>
       </div>
+      <edit-profile-popup v-if="showEditProfilePopup" @close="closeEditProfilePopup"></edit-profile-popup>
     </div>
   </template>
-
-<script>
-import axios from '@/../src/axios';
-import router from '@/routes';
-import { mapGetters } from 'vuex';
-
-export default {
+  
+  <script>
+  import EditProfilePopup from './UserPic.vue';
+  import axios from '@/../src/axios';
+  import router from '@/routes';
+  import { mapGetters } from 'vuex';
+  
+  export default {
+    components: {
+    'edit-profile-popup': EditProfilePopup,
+    },
     data() {
-        return {
-            user: {
-                cpf: '',
-            }
-        };
+      return {
+        showEditProfilePopup: false,
+        currentProfileImageUrl: 'https://static.vecteezy.com/system/resources/previews/020/911/739/non_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png',
+        user: {
+          cpf: '',
+        },
+      };
     },
     computed: {
-        ...mapGetters('user', ['getUserName', 'getUserEmail', 'getUserDate']), // Mapeando os getters do módulo 'user'
+      ...mapGetters('user', ['getUserName', 'getUserEmail', 'getUserDate']), // Mapeando os getters do módulo 'user'
     },
     methods: {
-        deleteUser(cpf) {
-            axios.delete(`/User/${cpf}`)
-                .then(response => {
-                    this.user = response.data;
-                    window.alert('Usuário deletado com sucesso!');
-                    router.push(`/list-users`);
-                })
-                .catch(error => {
-                    console.error("Erro ao buscar detalhes do curso:", error);
-                });
-        },
-        confirmDeleteUser() {
-            const shouldDelete = window.confirm("Tem certeza de que deseja deletar este usuário?");
-
-            if (shouldDelete) {
-                this.deleteUser(this.user.cpf);
-            }
-        },
-    },
+      deleteUser(cpf) {
+        axios.delete(`/User/${cpf}`)
+          .then(response => {
+            this.user = response.data;
+            window.alert('Usuário deletado com sucesso!');
+            router.push(`/list-users`);
+          })
+          .catch(error => {
+            console.error("Erro ao buscar detalhes do curso:", error);
+          });
+      },
+      confirmDeleteUser() {
+        const shouldDelete = window.confirm("Tem certeza de que deseja deletar este usuário?");
+  
+        if (shouldDelete) {
+          this.deleteUser(this.user.cpf);
+        }
+      },
+    
+      openEditProfilePopup() {
+        this.showEditProfilePopup = true;
+      },
+      closeEditProfilePopup() {
+        this.showEditProfilePopup = false;
+      },
+  },
 };
-</script>
-
-<style scoped>
-.text-left{
+  </script>
+  
+  <style scoped>
+  .text-left {
     text-align: left;
-}
-.user-profile {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.user-details {
-  margin: 20px;
-  text-align: center;
-  border: 1px solid #ccc;
-  padding: 20px;
-  background-color: #000000;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-.button {
-  background-color: #007BFF;
-  color: #fff;
-  padding: 10px 20px;
-  margin: 10px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  text-decoration: none;
-  text-align: center;
-}
-
-.delete-button {
-  background-color: #FF5733;
-}
-
-.button:hover {
-  background-color: #0056b3;
-}
-
-button:hover.delete-button {
-  background-color: #cc3e22;
-}
-.profile-image {
-  width: 100px; /* Ajuste o tamanho da imagem conforme necessário */
-  height: 100px; /* Ajuste o tamanho da imagem conforme necessário */
-  border-radius: 50%; /* Torna a imagem circular (borda arredondada) */
-}
-</style>
+  }
+  .user-profile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .user-details {
+    margin: 20px;
+    text-align: center;
+    border: 1px solid #ccc;
+    padding: 20px;
+    background-color: #000000;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+  .button {
+    background-color: #007BFF;
+    color: #fff;
+    padding: 10px 20px;
+    margin: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    text-align: center;
+  }
+  .delete-button {
+    background-color: #FF5733;
+  }
+  .button:hover {
+    background-color: #0056b3;
+  }
+  button:hover.delete-button {
+    background-color: #cc3e22;
+  }
+  .profile-container {
+    position: relative;
+    display: inline-block;
+  }
+  .profile-image {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+  .edit-profile-icon {
+    position: absolute;
+    top: 0;
+    right: 0;
+    font-size: 20px;
+    cursor: pointer;
+  }
+  </style>
