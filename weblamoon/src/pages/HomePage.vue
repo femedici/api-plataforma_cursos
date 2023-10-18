5f9ea2b9fdeccbba552d60bfdb189f3ac03f6ab6
Mattok <mattokplays@gmail.com>
2023-10-18 10:53:23 -0300

    Fix package-lock

--- weblamoon/src/pages/HomePage.vue
+++ weblamoon/src/pages/HomePage.vue
@@ -1,70 +1,62 @@
 <template>
-    <v-parallax
-      class="object-cover"
-      src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
-    >
-      <div class="page">
-        <div class="content">
-          <div class="text-left">
-            <h1>LaMoon</h1>
-            <p>Onde o seu conhecimento não tem limite!</p>
-          </div>
-          <div class="imagem-com-relevo">
-            <img
-              class="imagem-com-relevo-inner"
-              src="https://images.unsplash.com/photo-1513258496099-48168024aec0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-            />
-          </div>
+  <v-parallax class="object-cover"
+    src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80">
+    <div class="page">
+      <div class="content">
+        <div class="text-left">
+          <h1>LaMoon</h1>
+          <p>Onde o seu conhecimento não tem limite!</p>
         </div>
-        <div class="card-list">
-          <router-link v-for="(card, index) in cards" :to="'/courses/' + card.link" :key="index">
-            <div class="card">
-              <div class="card-image">
-                <img
-                  class="card-image-inner"
-                  :src="card.imageURL"
-                  alt="Imagem do curso"
-                />
-              </div>
-              <div class="card-details">
-                <h3>{{ card.title }}</h3>
-                <p>{{ card.description }}</p>
-              </div>
-            </div>
-          </router-link>
+        <div class="imagem-com-relevo">
+          <img class="imagem-com-relevo-inner"
+            src="https://images.unsplash.com/photo-1513258496099-48168024aec0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" />
         </div>
       </div>
-    </v-parallax>
-  </template>
-  <script>
-  export default {
-    name: "App",
-    data() {
-      return {
-        cards: [
+      <div class="card-list">
+        <router-link v-for="(card, index) in cards" :to="'/courses/' + card.link" :key="index">
+          <div class="card">
+            <div class="card-image">
+              <img class="card-image-inner" :src="card.imageURL" alt="Imagem do curso" />
+            </div>
+            <div class="card-details">
+              <h3>{{ card.title }}</h3>
+              <p>{{ card.description }}</p>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </v-parallax>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      cards: [
         {
-            "title": "Desenvolvimento WEB",
-            "description": "Descrição do curso de DEV Web",
-            "link": "desweb",
-            "imageURL": "https://classic.exame.com/wp-content/uploads/2017/09/programac3a7c3a3o.jpg?quality=70&strip=info&w=1024"
+          "title": "Desenvolvimento WEB",
+          "description": "Descrição do curso de DEV Web",
+          "link": "desweb",
+          "imageURL": "https://classic.exame.com/wp-content/uploads/2017/09/programac3a7c3a3o.jpg?quality=70&strip=info&w=1024"
         },
         {
-            "title": "Manutenção de Computadores",
-            "description": "Descrição do curso de manutenção",
-            "link": "manu1",
-            "imageURL": "https://classic.exame.com/wp-content/uploads/2017/09/programac3a7c3a3o.jpg?quality=70&strip=info&w=1024"
+          "title": "Manutenção de Computadores",
+          "description": "Descrição do curso de manutenção",
+          "link": "manu1",
+          "imageURL": "https://classic.exame.com/wp-content/uploads/2017/09/programac3a7c3a3o.jpg?quality=70&strip=info&w=1024"
         },
         {
-            "title": "Manutenção de Celulares",
-            "description": "Descrição do curso de manutenção",
-            "link": "manu2",
-            "imageURL": "https://classic.exame.com/wp-content/uploads/2017/09/programac3a7c3a3o.jpg?quality=70&strip=info&w=1024"
+          "title": "Manutenção de Celulares",
+          "description": "Descrição do curso de manutenção",
+          "link": "manu2",
+          "imageURL": "https://classic.exame.com/wp-content/uploads/2017/09/programac3a7c3a3o.jpg?quality=70&strip=info&w=1024"
         }
-        ],
-      };
-    },
-  };
-  </script>
+      ],
+    };
+  },
+};
+</script>
 
 <style scoped>
 .page {
@@ -132,13 +124,15 @@
   max-width: 40%;
   margin-top: 30px;
 }
+
 .imagem-com-relevo-inner {
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
   border-radius: 8px;
   max-width: 90%;
   margin-top: 20px;
 }
-h1{
-    font-size:xx-large;
+
+h1 {
+  font-size: xx-large;
 }
 </style>
--- weblamoon/src/pages/User/UserView.vue
+++ weblamoon/src/pages/User/UserView.vue
@@ -1,128 +1,137 @@
 <template>
-    <div class="user-profile">
-      <div class="user-details">
-        <h1>Dados do Usuário</h1>
-        <div class="profile-container">
-            <img class="profile-image" :src="currentProfileImageUrl" alt="Foto de Perfil">
-          <span class="edit-profile-icon" @click="openEditProfilePopup">✎</span>
-        </div>
-        <p class="text-left"><strong>Nome do usuário:</strong> {{ getUserName }}</p>
-        <p class="text-left"><strong>Email do usuário:</strong> {{ getUserEmail }}</p>
-        <p class="text-left"><strong>Data de criação do usuário:</strong> {{ getUserDate }}</p>
+  <div class="user-profile">
+    <div class="user-details">
+      <h1>Dados do Usuário</h1>
+      <div class="profile-container">
+        <img class="profile-image" :src="currentProfileImageUrl" alt="Foto de Perfil">
+        <span class="edit-profile-icon" @click="openEditProfilePopup">✎</span>
       </div>
-  
-      <div class="button-container">
-        <router-link :to="'/alter-user/' + user.cpf" class="button">Alterar Usuário</router-link>
-        <button type="button" @click="confirmDeleteUser" class="button delete-button">Deletar Usuário</button>
-      </div>
-      <edit-profile-popup v-if="showEditProfilePopup" @close="closeEditProfilePopup"></edit-profile-popup>
+      <p class="text-left"><strong>Nome do usuário:</strong> {{ getUserName }}</p>
+      <p class="text-left"><strong>Email do usuário:</strong> {{ getUserEmail }}</p>
+      <p class="text-left"><strong>Data de criação do usuário:</strong> {{ getUserDate }}</p>
     </div>
-  </template>
+
+    <div class="button-container">
+      <router-link :to="'/alter-user/' + user.cpf" class="button">Alterar Usuário</router-link>
+      <button type="button" @click="confirmDeleteUser" class="button delete-button">Deletar Usuário</button>
+    </div>
+    <edit-profile-popup v-if="showEditProfilePopup" @close="closeEditProfilePopup"></edit-profile-popup>
+  </div>
+</template>
   
-  <script>
-  import EditProfilePopup from './UserPic.vue';
-  import axios from '@/../src/axios';
-  import router from '@/routes';
-  import { mapGetters } from 'vuex';
-  
-  export default {
-    components: {
+<script>
+import EditProfilePopup from './UserPic.vue';
+import axios from '@/../src/axios';
+import router from '@/routes';
+import { mapGetters } from 'vuex';
+
+export default {
+  components: {
     'edit-profile-popup': EditProfilePopup,
+  },
+  data() {
+    return {
+      showEditProfilePopup: false,
+      currentProfileImageUrl: 'https://static.vecteezy.com/system/resources/previews/020/911/739/non_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png',
+      user: {
+        cpf: '',
+      },
+    };
+  },
+  computed: {
+    ...mapGetters('user', ['getUserName', 'getUserEmail', 'getUserDate']), // Mapeando os getters do módulo 'user'
+  },
+  methods: {
+    deleteUser(cpf) {
+      axios.delete(`/User/${cpf}`)
+        .then(response => {
+          this.user = response.data;
+          window.alert('Usuário deletado com sucesso!');
+          router.push(`/list-users`);
+        })
+        .catch(error => {
+          console.error("Erro ao buscar detalhes do curso:", error);
+        });
     },
-    data() {
-      return {
-        showEditProfilePopup: false,
-        currentProfileImageUrl: 'https://static.vecteezy.com/system/resources/previews/020/911/739/non_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png',
-        user: {
-          cpf: '',
-        },
-      };
+    confirmDeleteUser() {
+      const shouldDelete = window.confirm("Tem certeza de que deseja deletar este usuário?");
+
+      if (shouldDelete) {
+        this.deleteUser(this.user.cpf);
+      }
     },
-    computed: {
-      ...mapGetters('user', ['getUserName', 'getUserEmail', 'getUserDate']), // Mapeando os getters do módulo 'user'
+
+    openEditProfilePopup() {
+      this.showEditProfilePopup = true;
     },
-    methods: {
-      deleteUser(cpf) {
-        axios.delete(`/User/${cpf}`)
-          .then(response => {
-            this.user = response.data;
-            window.alert('Usuário deletado com sucesso!');
-            router.push(`/list-users`);
-          })
-          .catch(error => {
-            console.error("Erro ao buscar detalhes do curso:", error);
-          });
-      },
-      confirmDeleteUser() {
-        const shouldDelete = window.confirm("Tem certeza de que deseja deletar este usuário?");
-  
-        if (shouldDelete) {
-          this.deleteUser(this.user.cpf);
-        }
-      },
-    
-      openEditProfilePopup() {
-        this.showEditProfilePopup = true;
-      },
-      closeEditProfilePopup() {
-        this.showEditProfilePopup = false;
-      },
+    closeEditProfilePopup() {
+      this.showEditProfilePopup = false;
+    },
   },
 };
-  </script>
+</script>
   
-  <style scoped>
-  .text-left {
-    text-align: left;
-  }
-  .user-profile {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .user-details {
-    margin: 20px;
-    text-align: center;
-    border: 1px solid #ccc;
-    padding: 20px;
-    background-color: #000000;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
-  .button {
-    background-color: #007BFF;
-    color: #fff;
-    padding: 10px 20px;
-    margin: 10px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    text-decoration: none;
-    text-align: center;
-  }
-  .delete-button {
-    background-color: #FF5733;
-  }
-  .button:hover {
-    background-color: #0056b3;
-  }
-  button:hover.delete-button {
-    background-color: #cc3e22;
-  }
-  .profile-container {
-    position: relative;
-    display: inline-block;
-  }
-  .profile-image {
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-  }
-  .edit-profile-icon {
-    position: absolute;
-    top: 0;
-    right: 0;
-    font-size: 20px;
-    cursor: pointer;
-  }
-  </style>
+<style scoped>
+.text-left {
+  text-align: left;
+}
+
+.user-profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.user-details {
+  margin: 20px;
+  text-align: center;
+  border: 1px solid #ccc;
+  padding: 20px;
+  background-color: #000000;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.button {
+  background-color: #007BFF;
+  color: #fff;
+  padding: 10px 20px;
+  margin: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  text-align: center;
+}
+
+.delete-button {
+  background-color: #FF5733;
+}
+
+.button:hover {
+  background-color: #0056b3;
+}
+
+button:hover.delete-button {
+  background-color: #cc3e22;
+}
+
+.profile-container {
+  position: relative;
+  display: inline-block;
+}
+
+.profile-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+}
+
+.edit-profile-icon {
+  position: absolute;
+  top: 0;
+  right: 0;
+  font-size: 20px;
+  cursor: pointer;
+}
+</style>
