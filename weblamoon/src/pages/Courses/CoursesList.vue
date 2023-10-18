913fdf578c538d785cfd7fd76e9a62cc8ef91a6b
Felipe Medici <ic1dem.contato@gmail.com>
2023-10-18 11:52:03 -0300

    1

--- weblamoon/src/pages/Courses/CoursesList.vue
+++ weblamoon/src/pages/Courses/CoursesList.vue
@@ -79,6 +79,90 @@
           <small>Last updated 3 mins ago</small>
         </div>
       </div>
+      <div
+        class="mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+        <a href="#!">
+          <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/city/042.webp"
+            alt="Palm Springs Road" />
+        </a>
+        <div class="p-6">
+          <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            Card title
+          </h5>
+          <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural
+            lead-in to additional content. This card has even longer content
+            than the first to show that equal height action.
+          </p>
+        </div>
+        <div
+          class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center dark:border-neutral-600 dark:text-neutral-50">
+          <small>Last updated 3 mins ago</small>
+        </div>
+      </div>
+      <div
+        class="mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+        <a href="#!">
+          <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/city/042.webp"
+            alt="Palm Springs Road" />
+        </a>
+        <div class="p-6">
+          <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            Card title
+          </h5>
+          <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural
+            lead-in to additional content. This card has even longer content
+            than the first to show that equal height action.
+          </p>
+        </div>
+        <div
+          class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center dark:border-neutral-600 dark:text-neutral-50">
+          <small>Last updated 3 mins ago</small>
+        </div>
+      </div>
+      <div
+        class="mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+        <a href="#!">
+          <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/city/042.webp"
+            alt="Palm Springs Road" />
+        </a>
+        <div class="p-6">
+          <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            Card title
+          </h5>
+          <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural
+            lead-in to additional content. This card has even longer content
+            than the first to show that equal height action.
+          </p>
+        </div>
+        <div
+          class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center dark:border-neutral-600 dark:text-neutral-50">
+          <small>Last updated 3 mins ago</small>
+        </div>
+      </div>
+      <div
+        class="mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
+        <a href="#!">
+          <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/city/042.webp"
+            alt="Palm Springs Road" />
+        </a>
+        <div class="p-6">
+          <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            Card title
+          </h5>
+          <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural
+            lead-in to additional content. This card has even longer content
+            than the first to show that equal height action.
+          </p>
+        </div>
+        <div
+          class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center dark:border-neutral-600 dark:text-neutral-50">
+          <small>Last updated 3 mins ago</small>
+        </div>
+      </div>
     </div>
   </v-container>
 </template>
