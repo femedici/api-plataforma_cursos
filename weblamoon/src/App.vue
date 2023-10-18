6cebdd43fa805ece38c594b596ae8eec51f6a5b3
Felipe Medici <ic1dem.contato@gmail.com>
2023-10-18 12:29:38 -0300

    Mudança de Cores Bases do Sistema

--- weblamoon/src/App.vue
+++ weblamoon/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-layout class="">
 
-    <v-navigation-drawer class="bg-[#101828]">
+    <v-navigation-drawer class="bg-[#0c1922]">
       <SideBar /> 
     </v-navigation-drawer>
 
-    <v-main class="">
+    <v-main class="bg-[121f28]">
       <router-view></router-view>
     </v-main>
   </v-layout>
