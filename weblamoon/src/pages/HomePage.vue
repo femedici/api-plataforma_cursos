149009fb5d0550d58d2b9c57d68a32b5583f06d6
Mattok <mattokplays@gmail.com>
2023-10-20 01:16:22 -0300

    Homepage 90%

--- weblamoon/src/pages/HomePage.vue
+++ weblamoon/src/pages/HomePage.vue
@@ -4,55 +4,57 @@
     <div class="page">
       <div class="content">
         <div class="text-left">
-          <h1>LaMoon</h1>
-          <p>Onde o seu conhecimento não tem limite!</p>
+          <v-img class="logoimg" src="@/assets/logoName.png"></v-img>
+          <h1 class="textlogo">Embarque nessa jornada de possibilidades e explore um<br>universo de conhecimento!</h1>
+          <div class="matrix-section">
+            <div class="matrix-container">
+              <div class="card-matrix">
+                <div class="row">
+                  <div class="card">
+                    <h3><v-icon icon="mdi-wifi" size="small"></v-icon> Dev WEB</h3>
+                    <v-divider></v-divider>
+                    <p>Aprenda o básico de tudo necessário para desenvolver uma aplicação web completa!</p>
+                  </div>
+                  <div class="card">
+                    <h3><v-icon icon="mdi-wifi" size="small"></v-icon> Dev WEB</h3>
+                    <v-divider></v-divider>
+                    <p>Aprenda o básico de tudo necessário para desenvolver uma aplicação web completa!</p>
+                  </div>
+                  <div class="card">
+                    <h3><v-icon icon="mdi-wifi" size="small"></v-icon> Dev WEB</h3>
+                    <v-divider></v-divider>
+                    <p>Aprenda o básico de tudo necessário para desenvolver uma aplicação web completa!</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="imagem-com-relevo">
           <img class="imagem-com-relevo-inner"
             src="https://images.unsplash.com/photo-1513258496099-48168024aec0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" />
         </div>
       </div>
-      <div class="card-list">
-        <router-link v-for="(card, index) in cards" :to="'/courses/' + card.link" :key="index">
-          <div class="card">
-            <div class="card-image">
-              <img class="card-image-inner" :src="card.imageURL" alt="Imagem do curso" />
-            </div>
-            <div class="card-details">
-              <h3>{{ card.title }}</h3>
-              <p>{{ card.description }}</p>
-            </div>
-          </div>
-        </router-link>
-      </div>
+    </div>
+
+    <div class="big-card">
+      <h1><strong>Para começar é simples.</strong></h1>
+      <v-divider></v-divider>
+      <p class="bc-description"><br>Crie uma conta<br>Escolha um curso<br>Se cadastre nele, ou<br>Vá para a aba
+        cursos<br>Clique em "Publicar Curso"<br>Preencha todos os campos<br>E aguarde o mesmo ser liberado!</p>
+      <br>
+      <v-divider></v-divider>
+      <h3>Demonstrou interesse? Por que não começa <strong><router-link :to="'/create-user/'">criando uma
+            conta?</router-link></strong></h3>
     </div>
   </v-parallax>
 </template>
 <script>
+
 export default {
   name: "App",
   data() {
     return {
-      cards: [
-        {
-          "title": "Desenvolvimento WEB",
-          "description": "Descrição do curso de DEV Web",
-          "link": "desweb",
-          "imageURL": "https://classic.exame.com/wp-content/uploads/2017/09/programac3a7c3a3o.jpg?quality=70&strip=info&w=1024"
-        },
-        {
-          "title": "Manutenção de Computadores",
-          "description": "Descrição do curso de manutenção",
-          "link": "manu1",
-          "imageURL": "https://classic.exame.com/wp-content/uploads/2017/09/programac3a7c3a3o.jpg?quality=70&strip=info&w=1024"
-        },
-        {
-          "title": "Manutenção de Celulares",
-          "description": "Descrição do curso de manutenção",
-          "link": "manu2",
-          "imageURL": "https://classic.exame.com/wp-content/uploads/2017/09/programac3a7c3a3o.jpg?quality=70&strip=info&w=1024"
-        }
-      ],
     };
   },
 };
@@ -72,67 +74,94 @@ export default {
   justify-content: center;
 }
 
+.logoimg {
+  margin-top: -200px;
+  margin-left: -170px;
+}
+
 .text-left {
   width: 50%;
   padding: 20px;
-  text-align: center;
+  text-align: left;
   align-self: center;
+  margin-top: 150px;
 }
 
-.card-list {
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-}
-
-.horizontal-cards {
-  display: flex;
-  overflow-x: auto;
-}
-
-.card {
-  display: flex;
-  border-radius: 8px;
-  flex-direction: column;
-  border: 1px solid #ccc;
-  margin: 10px;
-  padding: 10px;
-  max-width: 80%;
-  align-items: center;
-}
-
-.card-details {
-  flex: 1;
-  padding: 10px;
-  text-align: center;
-}
-
-.card-image {
-  flex: 1;
-  text-align: center;
-  padding: 10px;
-}
-
-.card-image-inner {
-  max-width: 100%;
+.textlogo {
+  margin-top: -120px;
+  margin-left: 90px;
 }
 
 .imagem-com-relevo {
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  transform: scale(1.1);
+  box-shadow: 0 0 0px rgba(0, 0, 0, 0.7);
+  transform: scale(1);
   border-radius: 12px;
   max-width: 40%;
-  margin-top: 30px;
+  margin-top: 55px;
 }
 
 .imagem-com-relevo-inner {
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  box-shadow: 0 15px 10px rgba(0, 0, 0, 0.5);
   border-radius: 8px;
-  max-width: 90%;
-  margin-top: 20px;
+  max-width: 100%;
+  margin-top: 80px;
 }
 
 h1 {
-  font-size: xx-large;
+  font-size: large;
+}
+
+.matrix-section {
+  text-align: center;
+  margin: 40px;
+}
+
+.matrix-container {
+  margin-left: -130px;
+  margin-top: 130px;
+  display: flex;
+  justify-content: left;
+}
+
+.card-matrix {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.row {
+  display: flex;
+  justify-content: center;
+  margin: 10px 0;
+}
+
+.card {
+  background-color: #092a49;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin: 10px;
+  margin-left: 50px;
+  flex: 1;
+  width: 200px;
+  /* Largura do cartão */
+  height: 200px;
+  /* Altura do cartão */
+}
+
+
+.big-card {
+  margin-top: 200px;
+  border: 1px solid #092a4985;
+  padding: 20px;
+  width: 100%;
+  height: 300px;
+  text-align: center;
+  background-color: #092a4985;
+}
+
+.bc-description {
+  margin-left: 20px;
+  text-align: center;
 }
 </style>
