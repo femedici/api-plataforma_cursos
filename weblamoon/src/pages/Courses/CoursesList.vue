b3e075faff5925b0cc82c76656919da8aad6b918
Felipe Medici <ic1dem.contato@gmail.com>
2023-10-23 13:17:17 -0300

    new home full texts

--- weblamoon/src/pages/Courses/CoursesList.vue
+++ weblamoon/src/pages/Courses/CoursesList.vue
@@ -1,7 +1,8 @@
 <template>
   <v-container fluid class="bg-sky-950/60 flex flex-col">
     <div class="flex items-end justify-between">
-      <span class="uppercase font-bold text-5xl bg-clip-text text-cyan-100">
+      <span
+        class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-cyan-100">
         Cursos Disponíveis
       </span>
       <v-btn to="/create-course" prepend-icon="mdi-plus-circle"
@@ -10,7 +11,7 @@
       </v-btn>
     </div>
     <v-card-text class="font-sans text-zinc-50 text-xl">
-      <p></p>- <a class="underline decoration-teal-300">Explore</a> todos os cursos presentes na plataforma
+      <p></p><a class="underline decoration-teal-300">Explore</a> todos os cursos presentes na plataforma
       LaMoon.
     </v-card-text>
   </v-container>
@@ -26,18 +27,20 @@
             src="https://cdn.elearningindustry.com/wp-content/uploads/2020/08/5-ways-to-improve-your-course-cover-design-1024x575.png"
             alt="Course" />
           =
+
+          <div class="p-6">
+            <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-100">
+              {{ item.title }}
+            </h5>
+            <p class="mb-4 text-base text-neutral-100">
+              {{ item.description }}
+            </p>
+          </div>
+          <div
+            class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center dark:border-neutral-100 text-neutral-100">
+            <small>Criado por {{ item.creator }}</small>
+          </div>
         </a>
-        <div class="p-6">
-          <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-100">
-            {{ item.title }}
-          </h5>
-          <p class="mb-4 text-base text-neutral-100">
-            {{ item.description }}
-          </p>
-        </div>
-        <div class="mt-auto border-t-2 border-neutral-100 px-6 py-3 text-center dark:border-neutral-100 text-neutral-100">
-          <small>Criado por {{ item.creator }}</small>
-        </div>
       </div>
     </div>
   </v-container>
--- weblamoon/src/pages/HomePage.vue
+++ weblamoon/src/pages/HomePage.vue
@@ -1,29 +1,31 @@
 <template>
   <section class="bg-gray-900 lg:pt-[120px] pb-10 lg:pb-20"
-    style="background-image: url('https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-size: cover; background-position: center;">
+    style="background-image: url('https://images.unsplash.com/photo-1690046793092-f8d634523264?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-size: cover; background-position: center;">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
       <div class="mr-auto place-self-center lg:col-span-7">
         <h1
           class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
           Bem vindo a La Moon</h1>
         <br>
-        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Welcome to
-          [Company Name], your gateway to unforgettable travel experiences. We are a leading travel and tour company
-          dedicated to creating exceptional journeys for adventurers, explorers, and wanderers like you.</p>
-        <br><br><br><br><br><br>
-        <a href="/list-courses"
-          class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-          Cursos Disponíveis
+        <p class="max-w-2xl mb-6 font-light text-teal-100 lg:mb-8 md:text-lg lg:text-xl">Bem-vindo ao nosso universo de
+          aprendizagem! Aqui, você encontrará a porta de entrada para uma jornada de conhecimento e crescimento.
+          Navegue pelos nossos cursos abrangentes e inspiradores, projetados para moldar o seu futuro e aprimorar as suas
+          habilidades.
+          Seja qual for o seu objetivo, estamos prontos para ajudá-lo a trilhar o caminho do sucesso. Junte-se a nós e
+          comece a sua jornada de aprendizado hoje!</p>
+        <br>
+        <p class="max-w-2xl mb-6 font-light text-teal-100 lg:mb-8 md:text-lg lg:text-xl">Para começar crie sua conta.</p>
+        <br><br>
+        <a href="/create-user"
+          class="inline-flex items-center  justify-center px-5 py-3 text-base font-medium text-center text-white border border-gray-300 rounded-lg hover:bg-teal-400 focus:ring-4 focus:ring-gray-100">
+          Criar Conta
           <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd"
               d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
               clip-rule="evenodd"></path>
           </svg>
         </a>
-        <a href="/create-user"
-          class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-          Criar Conta
-        </a>
+        <br><br><br><br>
       </div>
     </div>
   </section>
@@ -31,20 +33,15 @@
     <div class="container">
       <div class="flex flex-wrap justify-center -mx-4">
         <div class="w-full px-4">
-          <div class="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
-            <h2 class="
-                font-bold
-                text-3xl
-                sm:text-4xl
-                md:text-[40px]
-                text-dark
-                mb-4
-                ">
-              Our Recent News
-            </h2>
-            <p class="text-base text-body-color">
-              There are many variations of passages of Lorem Ipsum available
-              but the majority have suffered alteration in some form.
+          <div class="text-center mx-auto mb-[60px] lg:mb-20 max-w-[610px]">
+            <div class="text-5xl font-extrabold ...">
+              <span class="bg-clip-text text-transparent bg-gradient-to-r from-sky-900 to-emerald-300">
+                Explore a Plataforma
+              </span>
+            </div>
+            <br>
+            <p class=" text-cyan-900 text-xl text-body-color">
+              Existem inúmeras formas de interagir com a La Moon. Veja alguns exemplos
             </p>
             <br>
             <br>
@@ -52,13 +49,13 @@
         </div>
       </div>
       <div class="flex flex-wrap justify-center -mx-4">
-        <div class="flex flex-wrap -mx-4">
-          <div class="w-full md:w-1/2 lg:w-1/3 px-4 hover:translate-y-[-0.3rem] hover:shadow-md"
-            v-for="course in courseCards" :key="course.id">
-            <a :href="course.link">
+        <div class="flex flex-wrap -mx-4" data-aos="fade-up">
+          <div class="w-full md:w-1/2 lg:w-1/3 px-4 hover:translate-y-[-0.3rem] hover:shadow-md" v-for="item in Cards"
+            :key="item.id">
+            <a :href="item.link">
               <div class="max-w-[370px] mx-auto mb-10 flex flex-col items-center">
                 <div class="rounded overflow-hidden mb-8">
-                  <img :src="course.image" :alt="course.title" class="w-full" />
+                  <img :src="item.image" :alt="item.title" class="w-full" />
                 </div>
                 <div>
                   <h3 class="font-semibold
@@ -70,10 +67,10 @@
                     inline-block
                     text-dark
                     hover:text-primary">
-                    {{ course.title }}
+                    {{ item.title }}
                   </h3>
                   <p class="text-base text-body-color">
-                    {{ course.description }}
+                    {{ item.description }}
                   </p>
                 </div>
               </div>
@@ -82,48 +79,51 @@
         </div>
       </div>
       <br><br>
-      <div class="flex flex-wrap justify-center -mx-4">
-        <div class="w-full px-4">
-          <div class="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
-            <a href="#" class="no-underline hover:underline justify-center">
-              <h2 class="
-                text-2xl
-                sm:text-2xl
-                md:text-[40px]
-                text-dark
-                mb-4
-                ">
-                Quero publicar um curso!
-              </h2>
-            </a>
-            <br>
-            <br>
-          </div>
-        </div>
-      </div>
     </div>
   </section>
   <section class="bg-cover bg-center py-32 w-full">
-    <div class="container mx-auto text-left text-white">
+    <div class="container mx-auto text-white">
       <div class="flex items-center">
-        <div class="">
-          <div data-aos="fade-down">
-            <h1 class="text-5xl font-medium mb-6 text-neutral-50">Sobre Nós</h1>
-            <p class="text-xl mb-12 text-white break-all">Nossa plataforma vai permitir que você compre cursos sobre os
-              tópicos que mais lhe interessam.
-              Mas a melhor parte é que, se você é um especialista ou tem conhecimento valioso a compartilhar, você pode
-              criar seu próprio curso e vendê-lo.
-              É uma oportunidade única para todos, sejam alunos ávidos ou futuros instrutores. Mas claro, todos os cursos
-              são verificados antes de aprovados,
-              além dos usuarios avaliarem a qualidade dos mesmos.
-              Estamos desenvolvendo essa plataforma como parte de nosso projeto para a matéria de Projeto Integrador da
-              UTFPR-CM no segundo semestre de 2023.</p>
-            <p class="text-xl mb-12 text-white break-all text-left">Nossa missão é capacitar indivíduos através da
-              educação, tornando o conhecimento e as habilidades acessíveis a todos.
-              Acreditamos que a educação é a chave para desbloquear oportunidades ilimitadas e estamos comprometidos em
-              tornar a
-              aprendizagem online eficaz e envolvente.</p>
-          </div>
+        <!-- Text content -->
+        <div class="w-2/3 mr-8" data-aos="fade-down">
+          <h1 class="text-5xl font-medium mb-6 text-neutral-50">Sobre Nós</h1>
+          <p class="text-xl mb-12 text-white break-all">
+            Nossa plataforma tem um objetivo claro: simplificar a vida daqueles que desejam aprender algo novo e também
+            daqueles que desejam compartilhar seu conhecimento.
+            No La Moon, quer você seja um indivíduo buscando aprender ou uma empresa que deseja oferecer cursos e
+            especializações,
+            fornecemos um espaço unificado para acomodar todas as suas necessidades de aprendizado.
+            Aqui, a diversidade de cursos e especializações está ao alcance de um clique, independentemente de você ser
+            uma pessoa física com um interesse pessoal
+            ou uma organização com experiência a compartilhar. Unimos todos esses recursos em um só lugar, tornando a
+            jornada de ensino e
+            aprendizado mais acessível e conveniente para todos os envolvidos. É a combinação perfeita de conhecimento e
+            oportunidade, ao alcance de todos..
+          </p>
+          <p class="text-xl mb-12 text-white break-all text-left">
+            Na La Moon, garantimos a qualidade e confiabilidade de
+            nossos cursos através de uma equipe dedicada que aprova apenas criadores qualificados.
+            Para usuários que desejam compartilhar seu conhecimento, o processo é descomplicado: basta fazer uma
+            solicitação e escolher um plano de assinatura adequado,
+            seja para um único curso ou múltiplos, adaptando-se às suas necessidades. Além disso, para empresas,
+            oferecemos contratos de serviço personalizados,
+            para atender às demandas corporativas. Dessa forma, garantimos que o aprendizado seja acessível, seguro e
+            adaptado às diferentes necessidades de nossos usuários,
+            tornando a La Moon uma plataforma versátil e confiável para aprimoramento pessoal e profissional.
+          </p>
+          <p class="text-xl mb-12 text-white break-all">
+            Para você, usuário, a La Moon oferece opções flexíveis. Você pode explorar nossa ampla variedade de cursos
+            gratuitos, obtendo conhecimento valioso sem custo algum.
+            Além disso, se desejar aprofundar seus estudos em áreas mais especializadas, temos cursos disponíveis a
+            preços acessíveis. Dessa forma, garantimos que a aprendizagem
+            esteja ao alcance de todos, independentemente do seu orçamento. Na La Moon, a sua busca por conhecimento é
+            sempre flexível e adaptada às suas preferências e necessidades.
+          </p>
+        </div>
+        <div class="w-1/3">
+          <img
+            src="https://images.unsplash.com/photo-1599837565318-67429bde7162?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Image Alt Text" class="w-auto h-auto rounded-lg content-end" />
         </div>
       </div>
     </div>
@@ -171,24 +171,24 @@ export default {
           githubUsername: "Cast43",
         },
       ],
-      courseCards: [
+      Cards: [
         {
-          title: "Curso",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          title: "Cursos Diversos",
+          description: " Este é o ponto de partida para personalizar sua nova jornada de aprendizado. Explore todos os cursos disponíveis no sistema",
           image: "https://images.pexels.com/photos/374897/pexels-photo-374897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          link: "/view-course/11750"
+          link: "/list-courses"
         },
         {
-          title: "How to earn more money as a wellness coach",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          title: "Se tornar um criador",
+          description: "Acha que seu conhecimento tem valor e pode ser agregado a mais pessoas? Veja mais como publicar um curso no La Moon",
           image: "https://images.pexels.com/photos/4974920/pexels-photo-4974920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          link: "/view-course/77238"
+          link: "#"
         },
         {
-          title: "The no-fuss guide to upselling and cross selling",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          title: "Seu espaço de aprendizagem",
+          description: "Todos seus cursos inscritos e seu progresso particular em cada um deles em um só espaço.",
           image: "https://images.pexels.com/photos/3747156/pexels-photo-3747156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          link: "/view-course/50938"
+          link: "#"
         }
       ]
     };
