feebe46ed2dd5a09d14e7c5abff1cd35dd65cbe4
Felipe Medici <ic1dem.contato@gmail.com>
2023-10-25 16:11:41 -0300

    resize for userview, alerts

--- weblamoon/src/components/LoginAlert.vue
+++ weblamoon/src/components/LoginAlert.vue
@@ -3,7 +3,7 @@
         <v-sheet elevation="12" max-width="600" rounded="lg" width="100%" class="pa-4 text-center mx-auto ">
             <v-icon class="mb-5" color="success" icon="mdi-check-circle" size="112"></v-icon>
             
-            <h2 class="text-h5 mb-6">{{ message }}</h2>
+            <h2 class="text-h5 mb-6"><br>{{ message }}</h2>
             
             <p class="mb-4 text-medium-emphasis text-body-2">
                 <a>Login realizado com sucesso!</a>
